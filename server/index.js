c2a7ccd80a5d2debb0f98d8a70cf4cf65f36891e
mahmud035 <mahamudulhasan4148@gmail.com>
2022-11-08 11:17:23 +0600

    connected with mongodb database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 require('colors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -8,6 +10,26 @@ const port = process.env.PORT || 5000;
 //* Middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yeflywl.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const dbConnect = async () => {
+  try {
+    await client.connect();
+    console.log('Database connected'.yellow.italic);
+  } catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+  }
+};
+
+dbConnect();
 
 app.get('/', (req, res) => {
   res.send('Nurturing Minds Server Running');
