a29e96d16b534af92a363bd66fc949fd1d495f13
mahmud035 <mahamudulhasan4148@gmail.com>
2022-11-08 12:25:38 +0600

    added service details page and show service details data

--- client/src/Pages/Home/Services/ServiceDetails/ServiceDetails.js
+++ client/src/Pages/Home/Services/ServiceDetails/ServiceDetails.js
@@ -4,11 +4,21 @@ import './ServiceDetails.css';
 
 const ServiceDetails = () => {
   const service = useLoaderData() || {};
+  const { _id, serviceName, price, imageURL, description } = service;
   console.log(service);
 
   return (
-    <div>
-      <p>Service Details</p>
+    <div className="container pt-5">
+      <div className="service-details-card card mb-3">
+        <img src={imageURL} className=" card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{serviceName}</h5>
+          <p className="card-text">{description}</p>
+          <h3 className="py-3">
+            Price: <span>{price}</span>
+          </h3>
+        </div>
+      </div>
     </div>
   );
 };
