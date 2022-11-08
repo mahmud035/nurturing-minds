b2d13b1c046df289362eda370ca5f67762a5cf77
mahmud035 <mahamudulhasan4148@gmail.com>
2022-11-08 14:24:49 +0600

    get resquest for all service data data and individual service data

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('colors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -31,8 +31,34 @@ const dbConnect = async () => {
 
 dbConnect();
 
+const servicesCollection = client.db('nurturingMindsDB').collection('services');
+
 app.get('/', (req, res) => {
   res.send('Nurturing Minds Server Running');
+});
+
+//* GET (READ)
+app.get('/few-service', async (req, res) => {
+  const query = {};
+  const cursor = servicesCollection.find(query);
+  const result = await cursor.limit(3).toArray();
+  res.send(result);
+});
+
+//* GET (READ)
+app.get('/services', async (req, res) => {
+  const query = {};
+  const cursor = servicesCollection.find(query);
+  const result = await cursor.toArray();
+  res.send(result);
+});
+
+//* GET (READ)
+app.get('/services/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: ObjectId(id) };
+  const service = await servicesCollection.findOne(query);
+  res.send(service);
 });
 
 app.listen(port, () => {
